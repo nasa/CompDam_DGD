8da8fcbb1afd79ebaa4cc2f7f3ee5edf18744281
Frank Leone <frank.a.leone@nasa.gov>
2018-10-02 09:53:11 -0400

    Additional comments in vucharlength()

--- for/vucharlength.for
+++ for/vucharlength.for
@@ -1,4 +1,4 @@
-Subroutine vucharlength(                                    &
+Subroutine vucharlength(  &
   ! Read only (unmodifiable) variables:
   nblock, nfieldv, nprops, ncomp, ndim, nnode, nstatev, kSecPt, kLayer, kIntPt, jElType, jElem,  &
   totalTime, stepTime, dt, cmname, coordMp, coordNode, direct, T, props, field, stateOld,        &
@@ -62,25 +62,31 @@ Subroutine vucharlength(                                    &
         edges_m(3, 3) = ABS(DOT_PRODUCT(edges(3, :), direct(k, :, 3)))
       End If
 
-      ! Determine which material directions the element edge vectors are closest to
-      thickDir = 3
+      ! Determine which sets of element edges the thickness, fiber, and matrix material directions are most closely aligned
+      !  Evaluate the thickness direction
+      thickDir = 3  ! Initial guess that the thickness material direction is most closely aligned with the global 3 direction
       If (ndim == 3) Then
         thick_test = MAX(edges_m(1,3)/Length(edges(1, :)), edges_m(2,3)/Length(edges(2, :)), edges_m(3,3)/Length(edges(3, :)))
         If (edges_m(1, 3)/Length(edges(1, :)) == thick_test) thickDir = 1
         If (edges_m(2, 3)/Length(edges(2, :)) == thick_test) thickDir = 2
       End If
-
-      fiberDir = MOD(thickDir, 3) + 1
+      !  Evaluate the fiber direction
+      fiberDir = MOD(thickDir, 3) + 1  ! Initial guess for the fiber material direction
       fiber_test = MAX(edges_m(1,1)/Length(edges(1, :)), edges_m(2,1)/Length(edges(2, :)), edges_m(3,1)/Length(edges(3, :)))
       If (edges_m(MOD(thickDir + 1, 3) + 1, 1)/Length(edges(MOD(thickDir + 1, 3) + 1, :)) == fiber_test) Then
         fiberDir = MOD(thickDir + 1, 3) + 1
       End If
-      matrixDir = 6 - fiberDir - thickDir
+      !  Evaluate the matrix direction
+      matrixDir = 6 - fiberDir - thickDir  ! The matrix material direction is the only unspoken for set of edges
 
+      ! delta_a_max is the maximum distance a crack may grow while passing through the element along the fiber direction
       delta_a_max = Length(edges(fiberDir, :)) * SIN(ACOS(DOT_PRODUCT(edges_n(fiberDir, :), edges_n(matrixDir, :)))) / &
                      SIN(ACOS(edges_m(matrixDir, 1) / Length(edges(matrixDir, :))))
+      ! delta_a_avg is the average distance a crack will grow when passing through the element along the fiber direction when it's
+      !  position along the height (i.e., matrix direction) of the element is unknown
       delta_a_avg = delta_a_max * edges_m(matrixDir, 2) / (edges_m(fiberDir, 2) + edges_m(matrixDir, 2))
 
+      ! Define the characteristic element lengths in the fiber (1), matrix (2), and thickness (3) material directions
       charLength(k, 1) = edges_m(fiberDir, 1)
       charLength(k, 2) = Length(edges(matrixDir, :)) * SIN(ACOS(DOT_PRODUCT(-edges_n(fiberDir, :), edges_n(matrixDir, :)))) / &
                           SIN(ACOS(edges_m(fiberDir, 2) / Length(edges(fiberDir, :))))
