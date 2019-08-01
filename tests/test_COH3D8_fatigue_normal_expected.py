475aa7030ab03162d9bee71e242a4002118f69e7
Frank Leone <frank.a.leone@nasa.gov>
2019-08-01 09:51:03 -0400

    SingleElement fatigue-to-static test
    Added a test to verify the cohesive displacement at which damage should 
    transition from fatigue growth to static growth

--- tests/test_COH3D8_fatigue_normal_expected.py
+++ tests/test_COH3D8_fatigue_normal_expected.py
@@ -1,8 +1,11 @@
 stress_ratio = 0.5
 strength = 62.3
 toughness = 0.277
+penalty = 1.e6
 
+delta_i = strength / penalty
 delta_f = 2.0 * toughness / strength
+delta_inflection_fatigue = delta_f - stress_ratio * (delta_f - delta_i)
 
 parameters = {
 	"results": [
@@ -49,6 +52,28 @@ parameters = {
             "zeroTol": strength * 0.001,  # Defines how close to zero the y value needs to be
             "referenceValue": delta_f,
             "tolerance": delta_f * 0.1
+        },
+        {
+            "type": "xy_infl_pt_bilinear",
+            "step": "Fatigue",
+            "identifier": [
+                { # x
+                    "symbol": "U3",
+                    "nset": "Z+",
+                    "position": "Node 5"
+                },
+                { # y
+                    "symbol": "S33",
+                    "elset": "COHESIVE",
+                    "position": "Element 1 Int Point 1"
+                }
+            ],
+            "window": [
+				delta_inflection_fatigue - (delta_inflection_fatigue - delta_i) * 0.80,
+				delta_inflection_fatigue + (delta_f - delta_inflection_fatigue) * 0.80
+				],
+            "referenceValue": [delta_inflection_fatigue, strength * stress_ratio],
+            "tolerance": [delta_inflection_fatigue * 0.01, strength * stress_ratio * 0.01],
         },
         {
             "type": "max",
--- tests/test_COH3D8_fatigue_shear13_expected.py
+++ tests/test_COH3D8_fatigue_shear13_expected.py
@@ -1,8 +1,11 @@
 stress_ratio = 0.5
 strength = 92.3
 toughness = 0.788
+penalty = 1.e6 * 0.277 * strength * strength / (toughness * 62.3 * 62.3)
 
+delta_i = strength / penalty
 delta_f = 2.0 * toughness / strength
+delta_inflection_fatigue = delta_f - stress_ratio * (delta_f - delta_i)
 
 parameters = {
 	"results": [
@@ -49,6 +52,28 @@ parameters = {
             "zeroTol": strength * 0.001,  # Defines how close to zero the y value needs to be
             "referenceValue": delta_f,
             "tolerance": delta_f * 0.05
+        },
+        {
+            "type": "xy_infl_pt_bilinear",
+            "step": "Fatigue",
+            "identifier": [
+                { # x
+                    "symbol": "U1",
+                    "nset": "Z+",
+                    "position": "Node 5"
+                },
+                { # y
+                    "symbol": "S13",
+                    "elset": "COHESIVE",
+                    "position": "Element 1 Int Point 1"
+                }
+            ],
+            "window": [
+				delta_inflection_fatigue - (delta_inflection_fatigue - delta_i) * 0.80,
+				delta_inflection_fatigue + (delta_f - delta_inflection_fatigue) * 0.80
+				],
+            "referenceValue": [delta_inflection_fatigue, strength * stress_ratio],
+            "tolerance": [delta_inflection_fatigue * 0.01, strength * stress_ratio * 0.01],
         },
         {
             "type": "max",
