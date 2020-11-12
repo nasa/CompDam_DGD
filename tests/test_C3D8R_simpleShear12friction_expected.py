5204f80b1dfcab46ba790f0540de37d73cde0b62
Frank Leone <frank.a.leone@nasa.gov>
2020-11-12 11:43:42 -0500

    Use derived assertions in simpleShear12friction

--- tests/test_C3D8R_simpleShear12friction_expected.py
+++ tests/test_C3D8R_simpleShear12friction_expected.py
@@ -1,3 +1,16 @@
+import math
+
+applied_compression = -40.0
+coefficient_of_friction = 0.3
+
+SL = 92.3  # 1--2 plane shear strength
+YC = 199.8  # matrix compressive strength
+G2C = 0.788  # Mode II matrix fracture toughness
+alpha0_DGD = 0.9080748  # matrix crack orientation due to pure matrix compression failure
+length = 0.1  # element edge length
+
+eta_L = -SL * math.cos(2*alpha0_DGD) / (YC * math.cos(alpha0_DGD) * math.cos(alpha0_DGD))
+
 parameters = {
 "results": [
         {
@@ -10,8 +23,8 @@ parameters = {
                     "elset": "ALL_ELEMS",
                     "position": "Element 1 Int Point 1"
                 },
-            "referenceValue": -40.7095,
-            "tolerance": 0.01
+            "referenceValue": applied_compression,
+            "tolerance": abs(applied_compression) * 0.005
         },
         {
             "type": "max",
@@ -22,8 +35,8 @@ parameters = {
                     "elset": "ALL_ELEMS",
                     "position": "Element 1 Int Point 1"
                 },
-            "referenceValue": 103.84,
-            "tolerance": 0.06
+            "referenceValue": SL - eta_L * applied_compression,
+            "tolerance": (SL - eta_L * applied_compression) * 0.005
         },
         {
             "type": "max",
@@ -34,8 +47,8 @@ parameters = {
                     "elset": "ALL_ELEMS",
                     "position": "Element 1 Int Point 1"
                 },
-            "referenceValue": 12.2,
-            "tolerance": 0.05
+            "referenceValue": -applied_compression * coefficient_of_friction,
+            "tolerance": abs(applied_compression) * coefficient_of_friction * 0.005
         },
         {
             "type": "min",
@@ -46,8 +59,14 @@ parameters = {
                     "elset": "ALL_ELEMS",
                     "position": "Element 1 Int Point 1"
                 },
-            "referenceValue": -12.2,
-            "tolerance": 0.05
+            "referenceValue": applied_compression * coefficient_of_friction,
+            "tolerance": abs(applied_compression) * coefficient_of_friction * 0.005
+        },
+        {
+            "type": "max",
+            "identifier": "Plastic dissipation: ALLPD for Whole Model",
+            "referenceValue": G2C * (length * length),  # Unrecoverable energy dissipation from fracture * fracture area: GSL*LC1*LC3
+            "tolerance": G2C * (length * length) * 0.005
         },
         {
             "type": "max",
